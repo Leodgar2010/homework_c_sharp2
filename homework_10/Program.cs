39fc08de6c6e49ca3e6fa1c60e1f655e75eed88e
Leodgar2010 <leodgar2010@yandex.ru>
2022-08-10 01:13:46 +0300

    Сделал все задания

--- homework_10/Program.cs
+++ homework_10/Program.cs
@@ -4,3 +4,16 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+Console.WriteLine ("Введите трёхзначное число: ");
+string? numberString1 = Console.ReadLine ();
+var number1=  int.Parse (numberString1!);
+if (number1<0) number1 = number1*-1;
+if (number1>999 || number1<100)
+{
+     Console.WriteLine ("введенное число не трехзначное");
+}
+else
+{   
+    number1 = (number1 % 100)/10;
+    Console.WriteLine ("Вторая цифра этого числа: "+number1);
+}
--- homework_13/Program.cs
+++ homework_13/Program.cs
@@ -3,3 +3,18 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+Console.WriteLine ("Введите число: ");
+string? numberString1 = Console.ReadLine ();
+var number1=  int.Parse (numberString1!);
+if (number1<0) number1 = number1*-1;
+if (number1>999)
+{
+    while (number1 > 999)
+    {
+       number1 = (number1/10);
+    }
+}
+if (number1<100)
+    Console.WriteLine ("третьей цифры нет");
+else
+    Console.WriteLine (number1%10);
